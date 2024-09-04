6391ccb8cd49d336dc04f379ee55d3eafb6070f6
Joda <JodaInteractive@protonmail.com>
2024-09-04 13:40:15 -0500

    refactor and cleanup

---
+++ README.md
@@ -0,0 +1,24 @@
+# A simple terminal dice roller built in Go
+
+## Install
+
+```
+go install github.com/jodainteractive/go-roller
+```
+
+## Use
+
+```sh
+go-roller 2d6
+
+
+# Output
+Rolling 2d6...
+Results:
+4
+1
+Total: 5
+Lowest: 1
+Highest: 4
+Average: 2
+```
--- main.go
+++ main.go
@@ -8,27 +8,38 @@ import (
 	"strconv"
 )
 
+var dieRegex = regexp.MustCompile(`(^\d+|)d(\d+$)`)
+
 func main() {
+	roller, quantity, sides := getRoller()
+	println("Rolling " + roller + "...")
+	println("Results:")
+	results := roll(quantity, sides)
+
+	if quantity > 1 {
+		printMultiRollStats(results)
+	}
+}
+
+// getRoller returns the roller, quantity, and sides of the die to roll
+// string - the roller
+// int - the quantity of dice to roll
+// int - the number of sides on the die
+func getRoller() (string, int, int) {
 	if len(os.Args) < 2 {
 		println("You did not provide a something to roll")
 		os.Exit(0)
 	}
 
 	roller := os.Args[1]
+	matches := dieRegex.FindStringSubmatch(roller)
+	if len(matches) < 3 || matches[2] == "" || matches[2] == "0" || matches[1] == "0" {
+		println("Invalid roller. Must match format '2d6' or simply 'd4' for a single roll.")
+		os.Exit(0)
+	}
 
-	roll(roller)
-}
-
-func roll(roller string) {
-	println("Rolling " + roller + "...")
-
-	regexp := regexp.MustCompile(`(^\d+|)d(\d+$)`)
-	matches := regexp.FindStringSubmatch(roller)
-
-	quantity := 0
-	if matches[1] == "" {
-		quantity = 1
-	} else {
+	quantity := 1
+	if matches[1] != "" {
 		q, err := strconv.Atoi(matches[1])
 		if err != nil {
 			println("Invalid quantity")
@@ -37,52 +48,51 @@ func roll(roller string) {
 		quantity = q
 	}
 
-	sides := 0
-	if matches[2] == "" {
+	sides, err := strconv.Atoi(matches[2])
+	if err != nil {
 		println("Invalid sides")
 		os.Exit(0)
-	} else {
-		s, err := strconv.Atoi(matches[2])
-		if err != nil {
-			println("Invalid sides")
-			os.Exit(0)
-		}
-		sides = s
-	} 
-	
-	println("Results:")
-	results := []int{}
-	for i := 0; i < quantity; i++ {
-		result := roll_die(sides)
-		println(result)
-		results = append(results, result)
 	}
 
-	if quantity > 1 {
-		total := 0
-		for _, result := range results {
-			total += result
-		}
-		println("Total: " + strconv.Itoa(total))
-
-		lowest := math.MaxInt32
-		for _, result := range results {
-			if result < lowest {
-				lowest = result
-			}
-		}
-		println("Lowest: " + strconv.Itoa(lowest))
-
-		highest := 0
-		for _, result := range results {
-			if result > highest {
-				highest = result
-			}
-		}
-		println("Highest: " + strconv.Itoa(highest))
-	}
+	return roller, quantity, sides
 }
 
-func roll_die(sides int) int {
+// rolls the dice; prints and returns the result of each roll
+func roll(quantity int, sides int) []int {
+	results := []int{}
+	
+	for i := 0; i < quantity; i++ {
+		result := rollDie(sides)
+		results = append(results, result)
+		println(result)
+	}
+
+	return results
+}
+
+// rolls a single die with the given number of sides and returns the result
+func rollDie(sides int) int {
 	return rand.Intn(sides) + 1
+}
+
+// prints the total, lowest, highest, and average of the results of multiple rolls
+func printMultiRollStats(results []int) {
+	total := 0
+	lowest := math.MaxInt32
+	highest := 0
+	for _, result := range results {
+		total += result
+		if result < lowest {
+			lowest = result
+		}
+		if result > highest {
+			highest = result
+		}
+	}
+	average := total / len(results)
+
+	println("Total: " + strconv.Itoa(total))
+	println("Lowest: " + strconv.Itoa(lowest))
+	println("Highest: " + strconv.Itoa(highest))
+	println("Average: " + strconv.Itoa(average))
 }
